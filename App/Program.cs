3fbcb38c18c97dcd22c741fac2384e6b97e11970
johanssonjulis <johansson.julis@gmail.com>
2023-09-12 16:20:29 +0200

    Changed to an anonymous timer function.

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Net;
 using Timer = System.Timers.Timer;
 
@@ -8,41 +9,31 @@ namespace nsPing
     {
         static void Main(string[] args)
         {
-            //var connectionString = Environment.GetEnvironmentVariable("mongodb://localhost:27017/");
-            string url = "www.helo.se"; // Replace with the URL you want to resolve
+            string url = "www.helo.se";
 
             Timer timer = new Timer();
             timer.Interval = TimeSpan.FromSeconds(3).TotalMilliseconds;
-            // en anonym funktion som endast används, där vi anropar den, alltså till min timer. Eftersom den är async använder vi await före.
-            timer.Elapsed += TimerElapsed;
+            timer.Elapsed += (sender, e) =>
+            {
+                try
+                {
+                    IPHostEntry hostEntry = Dns.GetHostEntry(url);
+                    IPAddress[] iPAddresses = hostEntry.AddressList;
+
+                    System.Console.WriteLine($"IP adresses for {url}:");
+                    foreach (IPAddress iPAddress in iPAddresses)
+                    {
+                        System.Console.WriteLine(iPAddress);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"An error occured: {ex.Message}");
+                }
+            };
+
             timer.Start();
             Console.ReadLine();
-        }
-
-        public static void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            string url = "www.helo.se";
-            ResolveIPAddress(url);
-        }
-
-        public static void ResolveIPAddress(string url)
-        {
-
-            try
-            {
-                IPHostEntry hostEntry = Dns.GetHostEntry(url);
-                IPAddress[] iPAddresses = hostEntry.AddressList;
-
-                System.Console.WriteLine($"IP adresses for {url}:");
-                foreach (IPAddress iPAddress in iPAddresses)
-                {
-                    System.Console.WriteLine(iPAddress);
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine($"An error occured: {ex.Message}");
-            }
         }
     }
 }
